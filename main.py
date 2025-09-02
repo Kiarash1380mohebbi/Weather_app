5b128888d423553baa35ebc68f2eead541f1445d
Kiarash1380mohebbi <Kiarashmohebbi00@gmail.com>
2025-09-02 11:33:55 -0700

    Step-1
    تکمیل شدن فرانت اند اپلیکیشن

---
+++ main.py
@@ -0,0 +1,38 @@
+from customtkinter import *
+from sqlite3 import *
+
+
+win = CTk()
+win.title('Weather Gate')
+win.geometry("800x600+250+50")
+win.resizable(False,False)
+
+font_text = CTkFont(family='Arial',size=24,weight='normal')
+
+Frame_app = CTkFrame(win,750,580,15,fg_color="#FF0000")
+
+Fram_textbox = CTkFrame(Frame_app,530,560,fg_color="#E6FAF9")
+
+Frame_entry_and_button =CTkFrame(Frame_app,200,550,fg_color="#FFFFFF",border_width=2,
+                                 border_color="#FFFFFF",corner_radius=15)
+
+text_box = CTkTextbox(Fram_textbox,520,550,border_width=1,border_color="#FF5100")
+text_box.configure(state="disabled")
+
+entry_city_name = CTkEntry(Frame_entry_and_button,190,50,border_width=1,corner_radius=10,
+                           border_color="#FF0000",placeholder_text='name city'
+                           ,placeholder_text_color="#FF5100",font=('Arial',20))
+
+btn_search = CTkButton(Frame_entry_and_button,150,50,text='Search',corner_radius=15,fg_color="#FF5100",
+                       text_color="#FFFFFF",font=font_text)
+
+
+Frame_app.pack_configure(padx=10,pady=10)
+Fram_textbox.pack_configure(padx=10,pady=10,side='right')
+Frame_entry_and_button.pack_configure(padx=10,pady=10,side='left')
+
+text_box.pack_configure(padx=10,pady=10)
+entry_city_name.pack(padx=10,pady=10)
+btn_search.pack(padx=10,pady=10)
+
+win.mainloop()
---
+++ settings/URL.py
@@ -0,0 +1,5 @@
+# API key
+API_KEY_app = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=15cc8f5b012245f074d4057e50112674"
+
+# OpenWeather URL
+URL = f"https://api.openweathermap.org/data/2.5/weather?q={'a'}&appid={API_KEY_app}"
