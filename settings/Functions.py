10dd6390405090c6368ab4a0b2474eef126014bf
Kiarash1380mohebbi <Kiarashmohebbi00@gmail.com>
2025-09-02 22:16:40 -0700

    weather_app

---
+++ settings/Functions.py
@@ -0,0 +1,11 @@
+import path 
+from tkinter import *
+from requests import *
+
+
+def get_city_name(entry_city_name):
+    text = entry_city_name.get()
+    text = StringVar(text)
+    
+
+def search_btn():
